384b85a9ea905a13b7d2d5b29cab7820b1ff124d
Bojana Sofronovic <bojanasofronovic@gmail.com>
2016-02-23 18:21:14 +0100

    Update paintera za pravougaonik i zaobljeni pravougaonik.

--- src/model/painters/PravougaonikPainter.java
+++ src/model/painters/PravougaonikPainter.java
@@ -1,22 +1,24 @@
 package model.painters;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.GradientPaint;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 import model.Oblik;
 import model.OblikPainter;
 import model.elements.Pravougaonik;
+import model.pozicija.GridPozicija;
+import model.pozicija.KoordinatePozicija;
+import model.pozicija.StraneSvetaPozicija;
+import model.raspored.GridRaspored;
+import model.raspored.KoordinateRaspored;
+import model.raspored.Raspored;
+import model.raspored.StraneSveta;
+import model.raspored.StraneSvetaRaspored;
 
 public class PravougaonikPainter extends OblikPainter {
-
-	private boolean isColorSet;
-	private GradientPaint gradientPaint;
 
 	public PravougaonikPainter(Oblik oblik) {
 		super(oblik);
@@ -24,51 +26,93 @@ public class PravougaonikPainter extends OblikPainter {
 
 	@Override
 	public void paint(Graphics2D g) {
-
+		
 		Pravougaonik pravougaonik = (Pravougaonik) oblik;
-
-		if (pravougaonik.color == null) {
-			g.setPaint(Color.black);
+		
+		int a = pravougaonik.getA();
+		int b = pravougaonik.getB();
+		
+		if (pravougaonik.getColor() != null) {
+			g.setColor(pravougaonik.getColor());
 		} else {
-			isColorSet = true;
-			g.setPaint(pravougaonik.getColor());
+			g.setColor(Color.BLUE);
 		}
-
-		if (pravougaonik.stroke == null) {
-			g.setStroke(new BasicStroke(5f));
-			;
-		} else {
-			g.setStroke(pravougaonik.stroke);
-		}
-
+		
 		g.rotate(Math.toRadians(pravougaonik.getUgao()));
 
-		int ax = (int) pravougaonik.getPocetnaTacka().getX();
-		int ay = (int) pravougaonik.getPocetnaTacka().getY();
+		// u zavisnosti od rasporeda i pozicije, izracunaj tacke i iscrtaj oblik
+		Raspored raspored = pravougaonik.getPozicija().getRaspored();
+		if (raspored instanceof KoordinateRaspored) {
+			
+			KoordinatePozicija pozicija = (KoordinatePozicija) pravougaonik.getPozicija();
+			
+			int x1 = (int) pozicija.getX1();
+			int y1 = (int) pozicija.getY1();
+			
+			int x2 = (int) pozicija.getX2();
+			int y2 = (int) pozicija.getY2();
+			
+			int x3 = (int) pozicija.getX3();
+			int y3 = (int) pozicija.getY3();
+			
+			int x4 = (int) pozicija.getX4();
+			int y4 = (int) pozicija.getY4();
+			
+			KoordinateRaspored koordinate = (KoordinateRaspored) raspored;
+			Point2D tacka1 = koordinate.getTacka(x1, y1);
+			Point2D tacka2 = koordinate.getTacka(x2, y2);
+			Point2D tacka3 = koordinate.getTacka(x3, y3);
+			Point2D tacka4 = koordinate.getTacka(x4, y4);
+			
+			g.draw(new Line2D.Double(tacka1, tacka2));
+			g.draw(new Line2D.Double(tacka2, tacka3));
+			g.draw(new Line2D.Double(tacka3, tacka4));
+			g.draw(new Line2D.Double(tacka4, tacka1));
+			
+			g.fillPolygon(new int[] { x1, x2, x3, x4 }, new int[] { y1, y2, y3, y4 }, 4);
+			
+		} else if (raspored instanceof GridRaspored) {
+			
+			GridPozicija pozicija = (GridPozicija) pravougaonik.getPozicija();
+			
+			int brojReda = pozicija.getBrojReda();
+			int brojKolone = pozicija.getBrojKolone();
+			
+			GridRaspored grid = (GridRaspored) raspored;
+			Point2D tacka = grid.izracunajTackuZaCeliju(brojKolone, brojReda);
 
-		int bx = (int) pravougaonik.getKrajnjaTacka().getX();
-		int by = (int) pravougaonik.getPocetnaTacka().getY();
+			int x1 = (int) tacka.getX();
+			int y1 = (int) tacka.getY();
+			
+			Rectangle2D rectangle = new Rectangle2D.Double(x1, y1, a, b);
+			
+			g.draw(rectangle);
+			g.fill(rectangle);			
+		
+		} else if (raspored instanceof StraneSvetaRaspored) {
+			
+			StraneSvetaPozicija pozicija = (StraneSvetaPozicija) pravougaonik.getPozicija();		
+			
+			StraneSveta stranaSveta = pozicija.getStranaSveta();
 
-		int cx = (int) pravougaonik.getPocetnaTacka().getX();
-		int cy = (int) pravougaonik.getKrajnjaTacka().getY();
+			StraneSvetaRaspored straneSveta = (StraneSvetaRaspored) raspored;	
+			
+			Point2D tacka = straneSveta.getTackaPravougaonik(stranaSveta, pravougaonik);
+			
+			int x = (int) tacka.getX();
+			int y = (int) tacka.getY();
 
-		int dx = (int) pravougaonik.getKrajnjaTacka().getX();
-		int dy = (int) pravougaonik.getKrajnjaTacka().getY();
+			Rectangle2D rectangle = new Rectangle2D.Double(x, y, a, b);
+			
+			g.draw(rectangle);
+			g.fill(rectangle);
 
-		g.draw(new Line2D.Double(new Point2D.Double(ax, ay), new Point2D.Double(cx, cy)));
-		g.draw(new Line2D.Double(new Point2D.Double(cx, cy), new Point2D.Double(dx, dy)));
-		g.draw(new Line2D.Double(new Point2D.Double(dx, dy), new Point2D.Double(bx, by)));
-		g.draw(new Line2D.Double(new Point2D.Double(bx, by), new Point2D.Double(ax, ay)));
-
-		if (isColorSet) {
-			g.fillPolygon(new int[] { ax, bx, dx, cx }, new int[] { ay, by, dy, cy }, 4);
 		}
-
+		
 	}
 
 	@Override
 	public boolean isElementAt(Point2D pos) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
--- src/model/painters/ZaobljeniPravougaonikPainter.java
+++ src/model/painters/ZaobljeniPravougaonikPainter.java
@@ -2,11 +2,22 @@ package model.painters;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.RoundRectangle2D;
+import java.util.List;
+
+import model.elements.ZaobljeniPravougaonik;
+import model.pozicija.GridPozicija;
+import model.pozicija.KoordinatePozicija;
+import model.pozicija.StraneSvetaPozicija;
+import model.raspored.GridRaspored;
+import model.raspored.KoordinateRaspored;
+import model.raspored.Raspored;
+import model.raspored.StraneSveta;
+import model.raspored.StraneSvetaRaspored;
 import model.Oblik;
 import model.OblikPainter;
-import model.elements.ZaobljeniPravougaonik;
 
 public class ZaobljeniPravougaonikPainter extends OblikPainter {
 
@@ -16,26 +27,79 @@ public class ZaobljeniPravougaonikPainter extends OblikPainter {
 
 	@Override
 	public void paint(Graphics2D g) {
-
-		ZaobljeniPravougaonik pravougaonik = (ZaobljeniPravougaonik) oblik;
-
-		if (pravougaonik.color == null) {
-			g.setColor(Color.RED);
-		} else {
-			g.setColor(pravougaonik.getColor());
-		}
-
-		g.rotate(Math.toRadians(pravougaonik.getUgao()));
-
-		RoundRectangle2D rectangle = new RoundRectangle2D.Double(
-				pravougaonik.getLokacija().getX(), pravougaonik.getLokacija().getY(),
-				pravougaonik.getVisinaPravougaonika(),
-				pravougaonik.getSirinaPravougaonika(),
-				pravougaonik.getVisinaLuka(), 
-				pravougaonik.getSirinaLuka());
 		
-		g.draw(rectangle);
-		g.fill(rectangle);
+		ZaobljeniPravougaonik pravougaonik = (ZaobljeniPravougaonik) oblik;
+		
+		int a = pravougaonik.getA();
+		int b = pravougaonik.getB();
+		int visinaLuka = pravougaonik.getVisinaLuka();
+		int sirinaLuka = pravougaonik.getSirinaLuka();
+		
+		if (pravougaonik.getColor() != null) {
+			g.setColor(pravougaonik.getColor());
+		} else {
+			g.setColor(Color.BLUE);
+		}
+		
+		g.rotate(Math.toRadians(pravougaonik.getUgao()));
+		
+		// u zavisnosti od rasporeda i pozicije, izracunaj tacke i iscrtaj element
+		Raspored raspored = pravougaonik.getPozicija().getRaspored();
+		if (raspored instanceof KoordinateRaspored) {
+			
+			KoordinatePozicija pozicija = (KoordinatePozicija) pravougaonik.getPozicija();
+			
+			int x = (int) pozicija.getX1();
+			int y = (int) pozicija.getY1();
+
+			KoordinateRaspored koordinate = (KoordinateRaspored) raspored;
+			Point2D tacka = koordinate.getTacka(x, y);
+			
+			int x1 = (int) tacka.getX();
+			int y1 = (int) tacka.getY();
+			
+			RoundRectangle2D roundRectangle = new RoundRectangle2D.Double(x1, y1, a, b, visinaLuka, sirinaLuka);
+			
+			g.draw(roundRectangle);
+			g.fill(roundRectangle);
+		
+		} else if (raspored instanceof GridRaspored) {
+			
+			GridPozicija pozicija = (GridPozicija) pravougaonik.getPozicija();
+			
+			int brojReda = pozicija.getBrojReda();
+			int brojKolone = pozicija.getBrojKolone();
+			
+			GridRaspored grid = (GridRaspored) raspored;
+			Point2D tacka = grid.izracunajTackuZaCeliju(brojKolone, brojReda);
+			
+			int x = (int) tacka.getX();
+			int y = (int) tacka.getY();
+			
+			RoundRectangle2D roundRectangle = new RoundRectangle2D.Double(x, y, a, b, visinaLuka, sirinaLuka);
+			
+			g.draw(roundRectangle);
+			g.fill(roundRectangle);
+			
+		} else if (raspored instanceof StraneSvetaRaspored) {
+			
+			StraneSvetaPozicija pozicija = (StraneSvetaPozicija) pravougaonik.getPozicija();
+			
+			StraneSveta stranaSveta = pozicija.getStranaSveta();
+
+			StraneSvetaRaspored straneSveta = (StraneSvetaRaspored) raspored;	
+			
+			Point2D tacka = straneSveta.getTackaZaobljeniPravougaonik(stranaSveta, pravougaonik);
+			
+			int x = (int) tacka.getX();
+			int y = (int) tacka.getY();
+
+			RoundRectangle2D roundRectangle = new RoundRectangle2D.Double(x, y, a, b, visinaLuka, sirinaLuka);
+			
+			g.draw(roundRectangle);
+			g.fill(roundRectangle);
+
+		}
 	}
 
 	@Override
