3c4676d547e3d001d6c6c358f6894022024b6cd7
Bojana Sofronovic <bojanasofronovic@gmail.com>
2016-02-23 18:18:38 +0100

    Update paintera za elipsu.

--- src/model/painters/ElipsaPainter.java
+++ src/model/painters/ElipsaPainter.java
@@ -4,14 +4,19 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
-
 import model.Oblik;
 import model.OblikPainter;
 import model.elements.Elipsa;
+import model.pozicija.GridPozicija;
+import model.pozicija.KoordinatePozicija;
+import model.pozicija.StraneSvetaPozicija;
+import model.raspored.GridRaspored;
+import model.raspored.KoordinateRaspored;
+import model.raspored.Raspored;
+import model.raspored.StraneSveta;
+import model.raspored.StraneSvetaRaspored;
 
 public class ElipsaPainter extends OblikPainter {
-
-	private boolean isColorSet;
 
 	public ElipsaPainter(Oblik oblik) {
 		super(oblik);
@@ -19,28 +24,77 @@ public class ElipsaPainter extends OblikPainter {
 
 	@Override
 	public void paint(Graphics2D g) {
-
-		Elipsa elipsa = (Elipsa) oblik;
-
-		if (elipsa.color == null) {
-			g.setColor(Color.BLACK);
-		} else {
-			g.setColor(elipsa.getColor());
-		}
-
-		if (elipsa.stroke != null) {
-			g.setStroke(elipsa.getStroke());
-		}
-
-		Ellipse2D ellipse = new Ellipse2D.Double(
-				elipsa.getGornjiLeviUgao().getX(), 
-				elipsa.getGornjiLeviUgao().getY(), 
-				elipsa.getSirina(),
-				elipsa.getVisina());
 		
-		g.draw(ellipse);
-		g.fill(ellipse);
+		Elipsa elipsa = (Elipsa) oblik;
+		
+		int precnik1 = elipsa.getPrecnik1();
+		int precnik2 = elipsa.getPrecnik2();
+		
+		if (elipsa.getColor() != null) {
+			g.setColor(elipsa.getColor());
+		} else {
+			g.setColor(Color.BLUE);
+		}
+		
+		g.rotate(Math.toRadians(elipsa.getUgao()));
+		
+		// u zavisnosti od rasporeda i pozicije, izracunaj tacke i iscrtaj element
+		
+		Raspored raspored = elipsa.getPozicija().getRaspored();
+		if (raspored instanceof KoordinateRaspored) {
 
+			KoordinatePozicija pozicija = (KoordinatePozicija) elipsa.getPozicija();
+			
+			int x = (int) pozicija.getX1();
+			int y = (int) pozicija.getY1();
+
+			KoordinateRaspored koordinate = (KoordinateRaspored) raspored;
+			Point2D tacka = koordinate.getTacka(x, y);
+			
+			int x1 = (int) tacka.getX();
+			int y1 = (int) tacka.getY();
+			
+			Ellipse2D elipse = new Ellipse2D.Double(x1, y1, precnik1, precnik2);
+			
+			g.draw(elipse);
+			g.fill(elipse);
+		
+		} else if (raspored instanceof GridRaspored) {
+			
+			GridPozicija pozicija = (GridPozicija) elipsa.getPozicija();
+			
+			int brojReda = pozicija.getBrojReda();
+			int brojKolone = pozicija.getBrojKolone();
+			
+			GridRaspored grid = (GridRaspored) raspored;
+			Point2D tacka = grid.izracunajTackuZaCeliju(brojKolone, brojReda);
+			
+			int x = (int) tacka.getX();
+			int y = (int) tacka.getY();
+			
+			Ellipse2D elipse = new Ellipse2D.Double(x, y, precnik1, precnik2);
+			
+			g.draw(elipse);
+			g.fill(elipse);
+			
+		} else if (raspored instanceof StraneSvetaRaspored) {
+			
+			StraneSvetaPozicija pozicija = (StraneSvetaPozicija) elipsa.getPozicija();
+			
+			StraneSveta stranaSveta = pozicija.getStranaSveta();
+
+			StraneSvetaRaspored straneSveta = (StraneSvetaRaspored) raspored;	
+			
+			Point2D tacka = straneSveta.getTackaElipsa(stranaSveta, elipsa);
+			
+			int x = (int) tacka.getX();
+			int y = (int) tacka.getY();
+
+			Ellipse2D elipse = new Ellipse2D.Double(x, y, precnik1, precnik2);
+			
+			g.draw(elipse);
+			g.fill(elipse);
+		}
 	}
 
 	@Override
